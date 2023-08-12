7c82553f027d0777ebf41da3ddd804ea1b1bb4e2
Hector Padron <helpad93@gmail.com>
2023-08-12 23:48:38 +0000

    corregido

--- src/app.py
+++ src/app.py
@@ -5,22 +5,29 @@ todos = [{ "label": "My first task", "done": False }]
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    return jsonify(todos)
+    json_text = jsonify(todos)
+    return json_text
+
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
-    print("Incoming request with the following body", request_body)
     todos.append(request_body)
+    print("Incoming request with the following body", request_body)
+    # return 'Response for the POST todo'
     return jsonify(todos)
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    print("This is the position to delete: ",position)
+    print("This is the position to delete: ", position)
+    if position < len(todos):
+        del todos[position]
+        return jsonify(todos)
+    else:
+        return 'Error de rango'
     
-    todos.pop((position-1))
     
-    return jsonify(todos)
+    
 
 
 
